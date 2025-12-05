91d85abda975f4cb5fe8ae36605455c6f44503b1
danny-fav <favndan@gmail.com>
2025-12-05 15:08:32 +0100

    first commit

--- app/components/LoginForm.jsx
+++ app/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import styles from "./page.module.css";
 
 function LoginForm() {
   const [username, setUsername] = useState("");
@@ -26,7 +27,7 @@ function LoginForm() {
       <label>Message</label><br></br><br></br>
       <textarea style={{fontSize:'16px', color:'#737373',borderBottom:'1px solid #737373',width:'530px', height:"130px"}} value={message} onChange={e => setMessage(e.target.value)} placeholder="Tell me about your project..."></textarea>
       <br /><br></br>
-      <button style={{width:'200px', display:'flex', alignItems:'center',gap:'15px', background:'linear-gradient(90deg, #C4A673 70%, #d6c6aaff 100%)',fontSize:'16px', padding:'10px 20px'}} type="submit"><p>SEND MESSAGE </p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-send w-4 h-4 group-hover:translate-x-1 transition-transform duration-300"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"></path><path d="m21.854 2.147-10.94 10.939"></path></svg></button>
+      <button className={styles.but2} style={{width:'200px', display:'flex', alignItems:'center',gap:'10px', background:'linear-gradient(90deg, #C4A673 70%, #d6c6aaff 100%)',fontSize:'16px', padding:'10px 20px'}} type="submit"><p>SEND MESSAGE </p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-send w-4 h-4 group-hover:translate-x-1 transition-transform duration-300"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"></path><path d="m21.854 2.147-10.94 10.939"></path></svg></button>
       <br></br>
       <p>{msg}</p>
     </form>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from "./page.module.css";
 const Navbar = () => {
   return (
     <div>
@@ -8,11 +9,11 @@ const Navbar = () => {
             <span style={{color:'#000', fontSize:'24px'}}>lexandra</span>
         </div>
         <div style={{display: 'flex', fontSize:'15px',position:'relative',right:'10px', float: 'right',gap: '30px'}}>
-                <a href="#home">HOME</a>
-                <a href="#about">ABOUT</a>
-                <a href="#work">WORK</a>
-                <a href="#services">SERVICES</a>
-                <a href="#contact">CONTACT</a>
+                <a href="#home" className={styles.navLink}>HOME</a>
+                <a href="#about" className={styles.navLink}>ABOUT</a>
+                <a href="#work" className={styles.navLink}>WORK</a>
+                <a href="#services" className={styles.navLink}>SERVICES</a>
+                <a href="#contact" className={styles.navLink}>CONTACT</a>
             </div>
       </nav>
     </div>
